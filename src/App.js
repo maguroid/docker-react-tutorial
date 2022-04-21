1a34b49942eca551acf68c0a6f26fc16990d30e8
maguro <maguro.devlife@gmail.com>
2022-04-21 20:37:26 +0900

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This text was changed
         </a>
       </header>
     </div>
